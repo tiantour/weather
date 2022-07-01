6fa6c42a15c25dc856cfae659e6a5e767f15210e
andy.jiang <150haomengting@163.com>
2022-07-01 17:14:26 +0800

    add interface{}

--- go.mod
+++ go.mod
@@ -12,5 +12,5 @@ require (
 	github.com/dustin/go-humanize v1.0.0 // indirect
 	github.com/golang/glog v0.0.0-20160126235308-23def4e6c14b // indirect
 	github.com/pkg/errors v0.9.1 // indirect
-	golang.org/x/sys v0.0.0-20220624220833-87e55d714810 // indirect
+	golang.org/x/sys v0.0.0-20220627191245-f75cf1eec38b // indirect
 )
--- weather.go
+++ weather.go
@@ -45,38 +45,38 @@ type (
 		Data       []*Data `json:"data,omitempty"`        // 每日数据
 	}
 	Data struct {
-		Day           string   `json:"day,omitempty"`           // 天
-		Date          string   `json:"date,omitempty"`          // 日期
-		Week          string   `json:"week,omitempty"`          // 星期
-		Wea           string   `json:"wea,omitempty"`           // 天气情况1
-		WeaImg        string   `json:"wea_img,omitempty"`       // 固定9种类型(您也可以根据wea字段自己处理)xue、lei、shachen、wu、bingbao、yun、yu、yin、qing
-		WeaDay        string   `json:"wea_day,omitempty"`       // 天气情况1
-		WeaDayImg     string   `json:"wea_day_img,omitempty"`   // 固定9种类型(您也可以根据wea字段自己处理)xue、lei、shachen、wu、bingbao、yun、yu、yin、qing
-		WeaNight      string   `json:"wea_night,omitempty"`     // 天气情况1
-		WeaNightImg   string   `json:"wea_night_img,omitempty"` // 固定9种类型(您也可以根据wea字段自己处理)xue、lei、shachen、wu、bingbao、yun、yu、yin、qing
-		Tem           string   `json:"tem,omitempty"`           // 实时温度
-		Tem1          string   `json:"tem1,omitempty"`          // 高温
-		Tem2          string   `json:"tem2,omitempty"`          // 低温
-		Humidity      string   `json:"humidity,omitempty"`      // 湿度
-		Visibility    string   `json:"visibility,omitempty"`    // 能见度
-		Pressure      string   `json:"pressure,omitempty"`      // 气压
-		Win           []string `json:"win,omitempty"`           // 风力
-		WinSpeed      string   `json:"win_speed,omitempty"`     // 风力等级
-		WinMeter      string   `json:"win_meter,omitempty"`     // 风力等级
-		Sunrise       string   `json:"sunrise,omitempty"`       // 日出
-		Sunset        string   `json:"sunset,omitempty"`        // 日落
-		Air           string   `json:"air,omitempty"`           // 空气质量
-		AirLevel      string   `json:"air_level,omitempty"`     // 空气质量等级
-		AirTips       string   `json:"air_tips,omitempty"`      // 空气质量描述
-		Phrase        string   `json:"phrase,omitempty"`        // 天气情况短语
-		Narrative     string   `json:"narrative,omitempty"`     // 天气情况描述
-		Moonrise      string   `json:"moonrise,omitempty"`      // 月出
-		Moonset       string   `json:"moonset,omitempty"`       // 月落
-		MoonPhrase    string   `json:"moonPhrase,omitempty"`    // 月相
-		Rain          string   `json:"rain,omitempty"`          // 降雨概率
-		UvIndex       string   `json:"uvIndex,omitempty"`       // 紫外线等级
-		UvDescription string   `json:"uvDescription,omitempty"` // 紫外线等级描述
-		Alarm         []*Alarm `json:"alarm,omitempty"`         // 预警
+		Day           string      `json:"day,omitempty"`           // 天
+		Date          string      `json:"date,omitempty"`          // 日期
+		Week          string      `json:"week,omitempty"`          // 星期
+		Wea           string      `json:"wea,omitempty"`           // 天气情况1
+		WeaImg        string      `json:"wea_img,omitempty"`       // 固定9种类型(您也可以根据wea字段自己处理)xue、lei、shachen、wu、bingbao、yun、yu、yin、qing
+		WeaDay        string      `json:"wea_day,omitempty"`       // 天气情况1
+		WeaDayImg     string      `json:"wea_day_img,omitempty"`   // 固定9种类型(您也可以根据wea字段自己处理)xue、lei、shachen、wu、bingbao、yun、yu、yin、qing
+		WeaNight      string      `json:"wea_night,omitempty"`     // 天气情况1
+		WeaNightImg   string      `json:"wea_night_img,omitempty"` // 固定9种类型(您也可以根据wea字段自己处理)xue、lei、shachen、wu、bingbao、yun、yu、yin、qing
+		Tem           string      `json:"tem,omitempty"`           // 实时温度
+		Tem1          string      `json:"tem1,omitempty"`          // 高温
+		Tem2          string      `json:"tem2,omitempty"`          // 低温
+		Humidity      string      `json:"humidity,omitempty"`      // 湿度
+		Visibility    string      `json:"visibility,omitempty"`    // 能见度
+		Pressure      string      `json:"pressure,omitempty"`      // 气压
+		Win           []string    `json:"win,omitempty"`           // 风力
+		WinSpeed      string      `json:"win_speed,omitempty"`     // 风力等级
+		WinMeter      string      `json:"win_meter,omitempty"`     // 风力等级
+		Sunrise       interface{} `json:"sunrise,omitempty"`       // 日出
+		Sunset        interface{} `json:"sunset,omitempty"`        // 日落
+		Air           string      `json:"air,omitempty"`           // 空气质量
+		AirLevel      string      `json:"air_level,omitempty"`     // 空气质量等级
+		AirTips       string      `json:"air_tips,omitempty"`      // 空气质量描述
+		Phrase        string      `json:"phrase,omitempty"`        // 天气情况短语
+		Narrative     string      `json:"narrative,omitempty"`     // 天气情况描述
+		Moonrise      interface{} `json:"moonrise,omitempty"`      // 月出
+		Moonset       interface{} `json:"moonset,omitempty"`       // 月落
+		MoonPhrase    interface{} `json:"moonPhrase,omitempty"`    // 月相
+		Rain          string      `json:"rain,omitempty"`          // 降雨概率
+		UvIndex       string      `json:"uvIndex,omitempty"`       // 紫外线等级
+		UvDescription string      `json:"uvDescription,omitempty"` // 紫外线等级描述
+		Alarm         []*Alarm    `json:"alarm,omitempty"`         // 预警
 	}
 	Alarm struct {
 		AlarmType    string `json:"alarm_type,omitempty"`    // 预警类型
@@ -93,6 +93,7 @@ func NewWeather() *Weather {
 
 func (w *Weather) FetchWithTTL(cityID string, cost int64, ttl time.Duration) (*Weather, error) {
 	cache.Wait()
+
 	result, ok := cache.Get(cityID)
 	if ok {
 		return result.(*Weather), nil
